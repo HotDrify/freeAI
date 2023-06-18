f35a1a1feae2ec6ba9aed9b1644efee75a947e2b
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-18 21:41:00 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-import setuptools
-
+from setuptools import setup, find_packages
 with open("README.md", "r", encoding = "utf-8") as fh:
-
     long_description = fh.read()
+    
 setuptools.setup(
   name = "freeAI",
   version = "1.0",
@@ -23,11 +22,13 @@ setuptools.setup(
   ],
   python_requires = ">=3.8",
   classifiers = [
-
-        "Programming Language :: Python :: 3",
-
-        "License :: OSI Approved :: MIT License",
-
-        "Operating System :: OS Independent",
-
-
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache License 2.0",
+  ],
+  packages=find_packages(),
+  install_requires=[
+    "requests",
+    "fake-useragent",
+  ]
+)
