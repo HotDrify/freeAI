69c0222731ce843422e6c7ad6eed029cfc8ada7b
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-18 23:52:54 +0300

    update __init__.py

--- freeAI/alpaca7b/__init__.py
+++ freeAI/alpaca7b/__init__.py
@@ -1,1 +1,34 @@
+"""
+@ Author : HotDrify
+@ Type : alpaca 7B
+@ GiT : https://github.com/HotDrify/freeAI
+"""
+import requests
+import time
+from fake_useragent import UserAgent
 
+headers = {
+  "User-Agent": UserAgent().random
+}
+class Running:
+    @staticmethod
+    def main(q, proxies = None):
+        r = requests.post(
+          "https://us-central1-arched-keyword-306918.cloudfunctions.net/run-inference-1",
+          proxies = proxies,
+          data = {
+            "prompt": q
+          }
+        )
+        
+        output = {
+          "created": time.time(),
+          "model": "alpaca-7b",
+          "result": [
+            {
+              "prompt": q,
+              "content": resp['completion']
+            }
+          ]
+        }
+        return output
