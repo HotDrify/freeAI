c384d6cbf1c6e72ccd716c7bf25e80350a790ed9
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-18 21:48:14 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding = "utf-8") as fh:
-    long_description = fh.read()
+    README = fh.read()
    
 setup(
   name = "freeAI",
