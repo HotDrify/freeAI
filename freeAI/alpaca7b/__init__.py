a2189a9d5987ac669cb78be442133ea571b133fa
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-19 00:09:43 +0300

    update __init__.py

--- freeAI/alpaca7b/__init__.py
+++ freeAI/alpaca7b/__init__.py
@@ -27,7 +27,7 @@ class Running:
           "result": [
             {
               "prompt": q,
-              "content": r.json()['completion']
+              "content": r.json()
             }
           ]
         }
