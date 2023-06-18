85517eebeac3deb473b2cd8bd644ba6c02a410ad
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-18 21:24:14 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+
+    long_description = fh.read()
+setuptools.setup(
+  name = "freeAI",
+  version = "1.0",
+  description = 
+  
