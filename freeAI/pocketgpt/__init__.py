aa3eae701597b9e919b0f61e0a599721240a0c65
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-20 15:39:03 +0300

    update __init__.py

--- freeAI/pocketgpt/__init__.py
+++ freeAI/pocketgpt/__init__.py
@@ -1,53 +1,58 @@
 """
 @ Author : HotDrify
-@ Type : AI-assist
+@ Type : GPT-4
 @ GiT : https://github.com/HotDrify/freeAI
 """
-import urllib.request
-import json
+import requests
+import time
 from fake_useragent import UserAgent
 
 headers = {
   "User-Agent": UserAgent().random,
-  "Content-type": "application/json"
+  "Accept": "*/*",
+  "Accept-Language": "ru-RU,ru;q==0.8,en-US;q=0.5,en;q=0.3",
+  "Content-Type": "application/json",
+  "Sec-Fetch-Deskt": "empty",
+  "Sec-Fetch-Mode": "cors",
+  "Sec-Fetch-Site": "same-origin"
 }
 class Running:
     @staticmethod
-    def main(
-        proxies = None,
-        systemMessage: str = "You are a helpful assistant",
-        q,
-        parentMessageId: str = "",
-        temperature: float = 0.8,
-        top_p: float = 1,
-    ):
-        json_data = {
-            "prompt": q,
-            "options": {"parentMessageId": parentMessageId},
-            "systemMessage": systemMessage,
-            "temperature": temperature,
-            "top_p": top_p,
-        }
-        req = urllib.request.Request(
-          "http://43.153.7.56:8080/api/chat-process",
-          data = {
-            "prompt": q,
-            "options": {
-              "parentMessageId": parentMessageId
-            },
-            "systemMessage": systemMessage,
-            "temperature": temperature,
-            "top_p": top_p
-          },
-          headers = headers
-          proxies = proxies
-        )
-        r = urllib.request.urlopen(req)
-        content = r.read().decode()
-        return Running.__get_json(content)
-
-    @classmethod
-    def __load_json(cls, content) -> dict:
-        split = content.rsplit("\n", 1)[1]
-        to_json = json.loads(split)
-        return to_json
+    def main(q, proxies = None):
+      json = {
+        "prompt": q,
+        "model": "gpt-4",
+        "plugin": "vanilla"
+      }
+      r = requests.post(
+        "http://pocketgpt.000webhostapp.com/api/chat/completions/",
+        headers = headers,
+        data = json,
+        proxies = proxies
+      )
+      if r.ok:
+          output = {
+            "status": ["OK"],
+            "created": time.time(),
+            "model": "pocketGPT-4",
+            "result": [
+              {
+                "prompt": q,
+                "content": r.json()['completion']
+              }
+            ]
+          }
+      else:
+          output = {
+            "status": [
+              {
+                "code": r.status_code
+              }
+            ],
+            "created": time.time(),
+            "model": "pocketGPT-4",
+            "result": [
+              {}
+            ]
+          }
+      return output
