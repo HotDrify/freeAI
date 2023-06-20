d752bd1c878819ed2642d1fe385dfd91a4b9e918
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-21 00:39:15 +0300

    update __init__.py

---
+++ freeAI/minigpt/__init__.py
@@ -0,0 +1,67 @@
+"""
+@ Author : HotDrify
+@ Type : gpt-3.5-turbo
+@ GiT : https://github.com/HotDrify/freeAI
+"""
+import asyncio
+import json
+import time
+from fake_useragent import UserAgent
+import aiohttp
+
+headers = {
+    "User-Agent": UserAgent().random,
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Content-Type": "application/json",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "123",
+    "Sec-Fetch-Site": "same-origin"
+}
+class Running:
+    @staticmethod
+    async def main(q, proxies=None):
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(
+                    "https://mflsf.aitianhu.fun/api/chat-process",
+                    json = {
+                      "prompt": q,
+                      "options": {},
+                      "systemMessage": "You are heplful gpt-3 assistant.",
+                      "temperature": 0.6,
+                      "top_p": 1
+                    },
+                    proxy = proxies
+            ) as response:
+                text = await response.text()
+                lines = text.splitlines()
+                for line in lines:
+                    data = json.loads(line)
+                    if "text" in data:
+                        out = data["text"]
+                if response.ok:
+                    output = {
+                        "status": ["OK"],
+                        "created": time.time(),
+                        "model": "GPT-3.5-turbo",
+                        "result": [
+                            {
+                                "prompt": q,
+                                "content": out
+                            }
+                        ]
+                    }
+                else:
+                    output = {
+                        "status": [
+                            {
+                                "code": response.status
+                            }
+                        ],
+                        "created": time.time(),
+                        "model": "GPT-3.5-turbo",
+                        "result": [
+                            {}
+                        ]
+                    }
+        return output
