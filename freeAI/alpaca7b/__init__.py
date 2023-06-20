e4a18b660d3515293d89489a4db17defa56a906c
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-20 16:13:49 +0300

    update __init__.py

--- freeAI/alpaca7b/__init__.py
+++ freeAI/alpaca7b/__init__.py
@@ -3,46 +3,49 @@
 @ Type : alpaca 7B
 @ GiT : https://github.com/HotDrify/freeAI
 """
-import requests
+import asyncio
 import time
 from fake_useragent import UserAgent
+import aiohttp
 
 headers = {
-  "User-Agent": UserAgent().random
+    "User-Agent": UserAgent().random
 }
+
 class Running:
     @staticmethod
-    def main(q, proxies = None):
-        r = requests.post(
-          "https://us-central1-arched-keyword-306918.cloudfunctions.net/run-inference-1",
-          proxies = proxies,
-          json = {
-            "prompt": q
-          }
-        )
-        if r.ok:
-            output = {
-              "status": ["OK"],
-              "created": time.time(),
-              "model": "alpaca-7B",
-              "result": [
-                {
-                  "prompt": q,
-                  "content": r.json()['completion']
-                }
-              ]
-            }
-        else:
-            output = {
-              "status": [
-                {
-                  "code": r.status_code
-                }
-              ],
-              "created": time.time(),
-              "model": "alpaca-7B",
-              "result": [
-                {}
-              ]
-            }
+    async def main(q, proxies=None):
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(
+                    "https://us-central1-arched-keyword-306918.cloudfunctions.net/run-inference-1",
+                    json = {
+                        "prompt": q
+                    },
+                    proxy = proxies
+            ) as response:
+                if response.ok:
+                    output = {
+                        "status": ["OK"],
+                        "created": time.time(),
+                        "model": "alpaca-7B",
+                        "result": [
+                            {
+                                "prompt": q,
+                                "content": await response.json()['completion']
+                            }
+                        ]
+                    }
+                else:
+                    output = {
+                        "status": [
+                            {
+                                "code": response.status
+                            }
+                        ],
+                        "created": time.time(),
+                        "model": "alpaca-7B",
+                        "result": [
+                            {}
+                        ]
+                    }
         return output
