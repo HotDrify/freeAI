70761675d3702e9adea70707d2712d263083a287
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-06-27 10:09:31 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
    
 setup(
   name = "freeAI",
-  version = "1.1.4",
+  version = "1.1.5",
   description = "this is a python package to use the completely free chatGPT and more models.",
   long_description = README,
   url = "https://github.com/HotDrify/freeAI",
